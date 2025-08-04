7c76d72f12cb1d406161ee3087d0ef36644b8932
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-03 22:59:09 -0500

    idk why this wasnt saved yet

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,63 +1,56 @@
-import { getWalkers, getCities } from "./database.js"
+import { getWalkers, getCities } from "./database.js";
 
-const walkers = getWalkers()
-const cities = getCities()
+const walkers = getWalkers();
+const cities = getCities();
 
-document.addEventListener(
-    'click',
-    (e) => {
-        const clickTarget = e.target
-        const clickId = clickTarget.dataset.cityId
+document.addEventListener("click", (e) => {
+  const clickTarget = e.target;
+  const clickId = clickTarget.dataset.cityId;
 
-        let walkerNames = ''
+  let walkerNames = "";
 
-        if(clickTarget.dataset.type === 'city'){
-        // for loop through walkers 
-        for (const walker of walkers) {
-            // if click id matches walker cityId
-            if(walker.cityId === parseInt(clickId)){
-                // add name to walker name
-                walkerNames += `${walker.name} `
-            }
-        }
-            window.alert(`${walkerNames} is servicing this city.`)
-        }
+  if (clickTarget.dataset.type === "city") {
+    // for loop through walkers
+    for (const walker of walkers) {
+      // if click id matches walker cityId
+      if (walker.cityId === parseInt(clickId)) {
+        // add name to walker name
+        walkerNames += `${walker.name} `;
+      }
     }
-)
-
+    window.alert(`${walkerNames} is servicing this city.`);
+  }
+});
 
 export const CityList = () => {
-    let citiesHTML = "<ul id=cityList>"
+  let citiesHTML = "<ul id=cityList>";
 
-    for (const city of cities) {
-        citiesHTML += `<li class="city-list" data-type='city'
+  for (const city of cities) {
+    citiesHTML += `<li class="city-list" data-type='city'
                             data-city-id="${city.id}"
                             >${city.city}
-                        </li>`
-    }
+                        </li>`;
+  }
 
-    citiesHTML += "</ul>"
+  citiesHTML += "</ul>";
 
-    return citiesHTML
-}
+  return citiesHTML;
+};
 
 // declare function
 export const sortCityList = () => {
-    const array = []
-    // grab the li from the Ul
-    const listItems = document.getElementsByClassName('city-list')
-    // convert li text to array
-    for (const item of listItems) {
-        array.push(item.innerHTML)
-    }
+  const array = [];
+  // grab the li from the Ul
+  const listItems = document.getElementsByClassName("city-list");
+  // convert li text to array
+  for (const item of listItems) {
+    array.push(item.innerHTML);
+  }
 
-    array.sort();
-    for(let i = 0; i < array.length; i++){
-        listItems[i].innerHTML = array[i]
-    }
-
-}
-
-
+  array.sort();
+  for (let i = 0; i < array.length; i++) {
+    listItems[i].innerHTML = array[i];
+  }
+};
 
 // sort li
