41cd90e7d28edadb33be17c676b4d0be2ec4b876
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-03 23:00:23 -0500

    added all assignments

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,40 +1,38 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers } from "./database.js";
 
 // Get copy of state for use in this module
-const pets = getPets()
-const walkers = getWalkers()
-
+const pets = getPets();
+const walkers = getWalkers();
 
 // Function whose responsibility is to find the walker assigned to a pet
 const findWalker = (pet, allWalkers) => {
-    let petWalker = null
+  let petWalker = null;
 
-    for (const walker of allWalkers) {
-        if (walker.id === pet.walkerId) {
-            petWalker = walker
-        }
+  for (const walker of allWalkers) {
+    if (walker.id === pet.walkerId) {
+      petWalker = walker;
     }
+  }
 
-    return petWalker
-}
+  return petWalker;
+};
 
 export const Assignments = () => {
-    let assignmentHTML = ""
-    assignmentHTML = "<ul>"
+  let assignmentHTML = "";
+  assignmentHTML = "<ul>";
 
-    for (const currentPet of pets) {
-        const currentPetWalker = findWalker(currentPet, walkers)
-        // plus equals???
-        assignmentHTML = `
+  for (const currentPet of pets) {
+    const currentPetWalker = findWalker(currentPet, walkers);
+    // plus equals???
+    assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
                 ${currentPetWalker.name} in ${currentPetWalker.city}
             </li>
-        `
-    }
+        `;
+  }
 
-    assignmentHTML += "</ul>"
+  assignmentHTML += "</ul>";
 
-    return assignmentHTML
-}
-
+  return assignmentHTML;
+};
